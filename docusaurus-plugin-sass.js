b41e1f20102d67de22ca3b702112c96fb220869b
Ramon Lamana <rlamana@gmail.com>
2020-03-06 17:37:20 +0100

    Add cache-loader

--- docusaurus-plugin-sass.js
+++ docusaurus-plugin-sass.js
@@ -2,13 +2,14 @@ module.exports = function() {
   return {
     name: 'docusaurus-plugin-sass',
     configureWebpack(_, isServer, utils) {
-      const { getStyleLoaders } = utils;
+      const { getStyleLoaders, getCacheLoader } = utils;
       return {
         module: {
           rules: [
             {
               test: /\.s[ac]ss$/i,
               use: [
+                getCacheLoader(isServer),
                 ...getStyleLoaders(isServer),
                 'sass-loader',
               ]

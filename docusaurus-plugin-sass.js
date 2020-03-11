9f98b5f71fd219826122d919127c1a4ec905ed8e
Ramon Lamana <rlamana@gmail.com>
2020-03-11 15:28:37 +0100

    Disable cache loader

--- docusaurus-plugin-sass.js
+++ docusaurus-plugin-sass.js
@@ -2,14 +2,13 @@ module.exports = function() {
   return {
     name: 'docusaurus-plugin-sass',
     configureWebpack(_, isServer, utils) {
-      const { getStyleLoaders, getCacheLoader } = utils;
+      const { getStyleLoaders } = utils;
       return {
         module: {
           rules: [
             {
               test: /\.s[ac]ss$/i,
               use: [
-                getCacheLoader(isServer),
                 ...getStyleLoaders(isServer),
                 'sass-loader',
               ]

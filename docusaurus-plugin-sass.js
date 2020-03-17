7057282bd13fb7283526d0e54f7dd3a2104fb968
Ramon Lamana <rlamana@gmail.com>
2020-03-17 19:24:02 +0100

    Support for SASS modules

--- docusaurus-plugin-sass.js
+++ docusaurus-plugin-sass.js
@@ -3,17 +3,33 @@ module.exports = function() {
     name: 'docusaurus-plugin-sass',
     configureWebpack(_, isServer, utils) {
       const { getStyleLoaders } = utils;
+      const isProd = process.env.NODE_ENV === 'production';
       return {
         module: {
-          rules: [
-            {
-              test: /\.s[ac]ss$/i,
+          rules: [{
+            test: /\.scss$/,
+            oneOf: [{
+              test: /\.module\.scss$/,
+              use: [
+                ...getStyleLoaders(isServer, {
+                  modules: {
+                    localIdentName: isProd
+                      ? `[local]_[hash:base64:4]`
+                      : `[local]_[path]`,
+                  },
+                  importLoaders: 1,
+                  sourceMap: !isProd,
+                  onlyLocals: isServer,
+                }),
+                'sass-loader'
+              ]
+            }, {
               use: [
                 ...getStyleLoaders(isServer),
-                'sass-loader',
+                'sass-loader'
               ]
-            }
-          ]
+            }]
+          }]
         }
       };
     }
